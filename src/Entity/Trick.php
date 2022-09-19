a0fcdd19613c4c9cb2e91101b4a8c02779df83b1
lauralazzaro <lz_laura@hotmail.com>
2022-09-19 18:31:11 +0200

    Issue #16 - verify same name for tricks

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -6,8 +6,13 @@ use App\Repository\TrickRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: TrickRepository::class)]
+#[UniqueEntity(
+    fields: ['name'],
+    message: 'Cannot have two tricks with the same name.'
+)]
 class Trick
 {
     #[ORM\Id]
