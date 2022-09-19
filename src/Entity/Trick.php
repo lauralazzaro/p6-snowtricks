b198dbf6a5630710321a8dddb02f9c5caaa312c3
lauralazzaro <lz_laura@hotmail.com>
2022-09-19 19:14:25 +0200

    Issue #12 : verify that name of trick is not blank in the entity

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TrickRepository::class)]
 #[UniqueEntity(
@@ -20,7 +21,8 @@ class Trick
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[ORM\Column(type: 'string', length: 255, unique: true, nullable: false)]
+    #[Assert\NotBlank]
     private $name;
 
     #[ORM\Column(type: 'text')]
